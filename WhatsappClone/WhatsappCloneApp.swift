472f36bd81b83436b49be64ab88dfd27b31e33a3
mertcancifter <mert_1998_03@hotmail.com.tr>
2022-11-24 15:58:32 +0300

    Initial Commit

---
+++ WhatsappClone/WhatsappCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsappCloneApp.swift
+//  WhatsappClone
+//
+//  Created by mert can çifter on 24.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct WhatsappCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
